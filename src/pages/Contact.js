f27c181e9cbc964795ef6c43c7927e5f03b6028d
Rajkumar184 <rajkumarmali184@gmail.com>
2022-10-28 00:49:36 +0500

    latest commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,7 +10,7 @@ const Contact = () => {
         <section className="topSection2" style={{ backgroundColor: "#F8F8F8" }}>
           <div className="container-lg">
             <div className="row justify-content-center">
-              <div className="col-12 col-md-8 col-xl-6 col-xxl-5 text-center">
+              <div className="col-12 col-md-8 col-xl-6 col-xxl-5 text-center ee">
                 <div className="mb-4">
                   <ContactForm />
                 </div>
